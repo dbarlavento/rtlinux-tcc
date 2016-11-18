62b19a74c318c0e00379fe3356f3a4da7e58c53d
dbarlavento <dbg@a.recife.ifpe.edu.br>
2016-11-18 16:56:25 -0300

    ao_rt_01.exe agora é um programa de tempo real.

--- audioRT_01/ao_rt_01.c
+++ audioRT_01/ao_rt_01.c
@@ -27,20 +27,35 @@
  * -lrt (não na versão RT)
  */
 
-#define BUFFER (1024 * 4) //Tamanho do buffer de saída - Não está sendo usado!
+#define TAM_PILHA (1024 * 4) //Tamanho da pilha do programa
 
-#define FREQ 440 //Frequência da senoide
+#define PRIORIDADE 99 //Prioridade do processo
+
+#define NSEG_POR_SEG (1000000000) //Resolução do timer
+
+#define FREQ 440 //Frequência da senoide, para saidas ciclicas
 
 #define NUM_CANAIS 2 //Número de canais de saída
 #define BIT_RESOLUCAO 16 //Número de bits de resolução da saída
 #define TX_AMOSTRAGEM 44100 //Taxa de amostragem da saída
 
-int pulso(int duracao, int sp_rate) {
-
+/* Inicializa a pilha com zeros  */
+void pilha_ini(void)
+{
+   unsigned char pilha[TAM_PILHA];
+   memset(pilha, 0, TAM_PILHA);
+   return;
 }
+
 
 int main(int argc, char* argv[])
 {
+   /* Variáveis para gestão de tempo real */
+   struct timespec t;
+   struct sched_param param;
+   int interval = 1000000000; /* 10ms */
+
+   /* Variáveis para gestão do drive de áudio */
    ao_device *device;
    ao_sample_format format;
    int default_driver;
@@ -51,16 +66,30 @@ int main(int argc, char* argv[])
    int i;
    float teste;
 
-   fprintf(stderr, "Áudio de tempo real - v.1\n");
+   /* Splash! */
+   fprintf(stderr, "Áudio de tempo real - v.4\n");
 
-   /* Inicialização do dispositivo de Áudio */
+   /* Configura o programa como de tempo real */
+   param.sched_priority = PRIORIDADE;
+   if(sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
+      perror("Falha ao configurar o escalonador.");
+      exit(-1);
+   }
 
+   /* Bloqueia os endereços de memória do processo na RAM */
+   if(mlockall(MCL_CURRENT|MCL_FUTURE) == -1) {
+      perror("Falha ao bloquear memória.");
+      exit(-2);
+   }
+
+   /* Inicializa a pilha do programa */
+   pilha_ini();
+
+   /* Inicialização do dispositivo de áudio */
    ao_initialize();
 
    /* Configuração do drive de áudio */
-
    default_driver = ao_default_driver_id();
-
    memset(&format, 0, sizeof(format));
    format.bits = BIT_RESOLUCAO;
    format.channels = NUM_CANAIS;
@@ -70,31 +99,28 @@ int main(int argc, char* argv[])
    /* Abre o drive de áudio */
    device = ao_open_live(default_driver, &format, NULL);
    if (device == NULL) {
-      fprintf(stderr, "Erro ao abrir o drive.\n");
-      return 1;
+      perror("Erro ao abrir o drive de áudio.");
+      exit(-3);
    }
 
-   /* -- Play some stuff -- */
+   /* Configura o tamanho do buffer */
    buf_size = format.bits/8 * format.channels * format.rate;
    buffer = calloc(buf_size, sizeof(char));
 
-   printf("Buffer = %d\n", buf_size);
-
+   /* Geração da saída */
    for (i = 0; i < format.rate; i++) {
 
-      teste =  ((float) i / format.rate);
-      //sample = (int)(10000 * sin(2 * M_PI * freq * ((float) i/format.rate)));
+      //teste =  ((float) i / format.rate);
+      sample = (int)(10000 * sin(2 * M_PI * freq * ((float) i/format.rate)));
 
       //printf("Teste %d = %f\n", i, teste);
 
       /* Gera uma onda quadrada simetrica */
-      /*
       if( sample > 0 ) {
          sample = 0x5fff;
       } else {
          sample = 0;
       }
-      */
 
       /* Gera um pulso de 100ms */
       /*
@@ -105,8 +131,10 @@ int main(int argc, char* argv[])
       }
       */
 
+
       /* Gera uma sequencia de pulsos (10011001), cada bit tem duração de 50ms */
       /* ESTA É UMA FORMA MUITO INEFICIENTE DE FAZER ISSO!!! */
+      /*
       if( teste < 0.05 ) {
          sample = 0x5fff;
       } else if(teste >= 0.15 && teste < 0.25) {
@@ -116,13 +144,32 @@ int main(int argc, char* argv[])
       } else {
          sample = 0;
       }
+      */
 
-      /* Atribui o mesmo sinal é atribuido a ambos os canais */
+      /* Atribui o mesmo sinal a ambos os canais */
       buffer[4*i] = buffer[4*i+2] = sample & 0xff;
       buffer[4*i+1] = buffer[4*i+3] = (sample >> 8) & 0xff;
    }
 
-   ao_play(device, buffer, buf_size);
+   /* Inicia a estrututura t com o tempo do sistema */
+   clock_gettime(CLOCK_MONOTONIC ,&t);
+
+   /* Loop principal */
+   while(1) {
+      /* Espera pela próxima execução */
+      clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, NULL);
+
+      /* Executa o pulso */
+      ao_play(device, buffer, buf_size);
+
+      /* calcula o tempo da próxima execução */
+      t.tv_nsec += interval;
+
+      while (t.tv_nsec >= NSEG_POR_SEG) {
+         t.tv_nsec -= NSEG_POR_SEG;
+         t.tv_sec++;
+      }
+   }
 
    /* Encerra o drive */
    ao_close(device);
